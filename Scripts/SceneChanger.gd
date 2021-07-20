d35a6ef66bca0828efbf2152f8c6048a6060d7dc
FireGamesDev <firedevgames@gmail.com>
2021-07-20 09:49:12 +0200

    Last touches

--- Scripts/SceneChanger.gd
+++ Scripts/SceneChanger.gd
@@ -6,5 +6,5 @@ func change_scene(path, delay = 0.5):
 	yield(get_tree().create_timer(delay), "timeout")
 	animation_player.play("fade")
 	yield(animation_player, "animation_finished")
-	assert(get_tree().change_scene(path) == OK)
+	get_tree().change_scene(path)
 	animation_player.play_backwards("fade")
